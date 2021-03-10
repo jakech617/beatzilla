a984730c6ebc4b39bd87967126b2d3c47a189392
Jake <jakech617@gmail.com>
2021-03-09 21:40:41 -0500

    got spotify api working and retrieving data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 // tastedive
-const tasteDiveApi = "404780-HeatherK-F0AYR43I";
+const tasteDiveApi = "404780-HeatherK-F0AYR43I"; 
 let tasteDiveUrl = "https://tastedive.com/api/similar?q=";
 let artistInputEl = document.querySelector('#artist');
 let artistContainerEl = document.querySelector('#artist-container');
 let userForm = document.querySelector("#user-form");
+const discography = document.getElementById('discogs')
+
+// spotify
+const spotifyId = "a7d89107396f413598b8ee57ef143c21";
+const spotifySecret = "a7dc91eae51d4a60b734bb9fb1423496";
+const spotifyApi = 'https://api.spotify.com/v1/search?type=album&q=';
+const redirectUri = "https://jakech617.github.io/beatzilla/callback";
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -23,24 +30,74 @@ var formSubmitHandler = function (event) {
 
 userForm.addEventListener("submit", formSubmitHandler);
 
-  function getArtists(artist) {
-      fetch(tasteDiveUrl + artist + "&k=" + tasteDiveApi).then(function (response) {
-          if (response.ok) {
-              return response.json();
-          }
-      }).then(function(data) {
-          console.log(data);
-      })
-  }
+function getArtists(artist) {
+    fetch(tasteDiveUrl + artist + "&k=" + tasteDiveApi).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+    }).then(function(data) {
+        console.log(data);
+    })
+};
 
+discography.addEventListener('click', async function() {
+    const token = await getSpotifyToken(spotifyId, spotifySecret)
 
-// var buttonClickHandler = function (event) {
-//     var language = event.target.getAttribute('data-language');
-  
-//     if (language) {
-//       getFeaturedRepos(language);
-  
-//       repoContainerEl.textContent = '';
-//     }
-//   };
+        const artist = await fetch(`https://api.spotify.com/v1/search?q=${artistInputEl.value}&type=artist`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(response => response.json())
+
+    return fetch(`https://api.spotify.com/v1/artists/${artist.artists.items[0].id}/top-tracks?market=US`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(response => response.json()).then(data => console.log(data)).catch(error => console.log(error));
+
+})
+
+function getSpotifyToken(clientId, clientSecret) {
+    const formData = new URLSearchParams();
+    formData.append('grant_type', 'client_credentials');
+    return fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+        },
+        body: formData.toString()
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        return data.access_token;
+    })
+}
+
+// getSpotifyToken(spotifyId, spotifySecret).then(token => {
+//     return fetch('https://api.spotify.com/v1/artists/a7d89107396f413598b8ee57ef143c21/top-tracks?market=US', {
+//         method: 'GET',
+//         headers: {
+//             Authorization: `Bearer ${token}`
+//         }
+//     }).then(response => response.json()).then(data => console.log(data))
+// });
+
+// getSpotifyToken(spotifyId, spotifySecret).then(function(token) {
+//     return fetch('https://api.spotify.com/v1/artists/0rvjqX7ttXeg3mTy8Xscbt/top-tracks/2TpxZ7JUBn3uw46aR7qd6V', {
+//         method: 'GET',
+//         headers: { 
+//              Authorization: `Bearer ${token}`
+//         }
+//     }).then(function(response) {
+//         return response.json();
+//     }).then(function(data) {
+//         console.log(data);
+//     })
+// })
+
 
