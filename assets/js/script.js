70002b771d5ebbe00874afc6c945064a9ff742d2
Jake <jakech617@gmail.com>
2021-03-09 19:07:21 -0500

    added spotify id, secret, api link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,46 @@
-var lyricApi = "https://www.stands4.com/services/v2/lyrics.php";
+// tastedive
+const tasteDiveApi = "404780-HeatherK-F0AYR43I";
+let tasteDiveUrl = "https://tastedive.com/api/similar?q=";
+let artistInputEl = document.querySelector('#artist');
+let artistContainerEl = document.querySelector('#artist-container');
+let userForm = document.querySelector("#user-form");
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    let artistSearch = artistInputEl.value.trim();
+
+    console.log(artistSearch);
+    if (artistSearch) {
+        getArtists(artistSearch);
+
+        artistContainerEl.textContent = '';
+        artistInputEl.value = '';
+    } else {
+        alert('Please enter a valid artist name');
+    }
+};
+
+userForm.addEventListener("submit", formSubmitHandler);
+
+  function getArtists(artist) {
+      fetch(tasteDiveUrl + artist + "&k=" + tasteDiveApi).then(function (response) {
+          if (response.ok) {
+              return response.json();
+          }
+      }).then(function(data) {
+          console.log(data);
+      })
+  }
+
+
+// var buttonClickHandler = function (event) {
+//     var language = event.target.getAttribute('data-language');
+  
+//     if (language) {
+//       getFeaturedRepos(language);
+  
+//       repoContainerEl.textContent = '';
+//     }
+//   };
+
